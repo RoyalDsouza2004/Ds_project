2ee13647ce748d168351d4bcc6b763471e08fa32
Royal dsouza <royaldsouza2004@gmail.com>
2023-01-15 21:13:10 +0530

    updated

--- using_link.c
+++ using_link.c
@@ -215,7 +215,7 @@ int main()
                         case 2: 
                             struct student *new;
                             printf("\n");
-                            for(int i=0 ; i< (rows * seats) ; i++)
+                            for(int i=0 ; i< (rows * 3) ; i++)
                                 printf("*");
                             printf("\n");
                             for(int i=0 ; i<rows ; i++)
@@ -227,7 +227,7 @@ int main()
                                 printf("\n\n");
                             }
                             printf("\n");
-                            for(int i=0 ; i< (rows * seats) ; i++)
+                            for(int i=0 ; i< (rows * 3) ; i++)
                                 printf("*");
                             n = 1;
 
